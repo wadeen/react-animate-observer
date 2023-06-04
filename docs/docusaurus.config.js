712b96bda4daae4ee41071dd5578f7e14ae8e029
wadeen <meister.vvdo@gmail.com>
2023-06-04 23:19:37 +0900

    :adhesive_bandage: Fix: typo opg & title

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/ogp.jpg',
+      image: 'img/ogp.png',
       navbar: {
         title: 'React Animate Observer',
         logo: {
